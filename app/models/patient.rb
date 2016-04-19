05e2adb675a3763d09231a29b8cc92354b2e5f97
Julian P <julian.pineda21@gmail.com>
2016-04-18 22:03:00 -0500

    validate_quantity records in rm and tac

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,7 +1,9 @@
 class Patient < ActiveRecord::Base
-
+  @@num_rms = 2
+  @@num_tacs = 2
   validates :name, :age, :identification, :modality, presence: true
   before_save :set_random_algorithm
+  validate :validate_quantity
 
   def set_random_algorithm
     array_rm = [{type_catheter: "NG", type_caliber: 20}, {type_catheter: "NG", type_caliber: 22},
@@ -14,7 +16,7 @@ class Patient < ActiveRecord::Base
 
       loop do
         random = rand(0..3)
-        if Patient.where(modality: "RM", catheter: array_rm[random][:type_catheter], caliber: array_rm[random][:type_caliber]).count < 25
+        if Patient.where(modality: "RM", catheter: array_rm[random][:type_catheter], caliber: array_rm[random][:type_caliber]).count < @@num_rms
           self.catheter = array_rm[random][:type_catheter]
           self.caliber = array_rm[random][:type_caliber]
           break
@@ -25,13 +27,21 @@ class Patient < ActiveRecord::Base
 
       loop do
         random = rand(0..3)
-        if Patient.where(modality: "TAC", catheter: array_tac[random][:type_catheter], caliber: array_tac[random][:type_caliber]).count < 175
+        if Patient.where(modality: "TAC", catheter: array_tac[random][:type_catheter], caliber: array_tac[random][:type_caliber]).count < @@num_tacs
           self.catheter = array_tac[random][:type_catheter]
           self.caliber = array_tac[random][:type_caliber]
           break
         end
       end
     end 
+  end
+
+  def validate_quantity
+    if modality == "RM"
+      errors.add(:modality, "El número de pacientes de RM está completo") if Patient.where(modality: "RM").count >= (@@num_rms*4)
+    else
+      errors.add(:modality, "El número de pacientes de TAC está completo") if Patient.where(modality: "TAC").count >= (@@num_tacs*4)
+    end
   end
 
   
